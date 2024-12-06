1ade148f7afb78200644c2ce1be485bdb6cd1426
dimitrije-savatic <dimitrije.savatic@gmail.com>
2024-12-06 21:37:18 +0100

    some changes

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,45 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component, Renderer2 } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private renderer: Renderer2, private route: ActivatedRoute) {}
 
-  constructor(private renderer: Renderer2){}
-
-  active: string = 'text-white'
+  active: string = 'text-white';
   burgerActive: boolean = false;
   XActive: boolean = true;
   showBurgerMenu: boolean = false;
   links: any = [
     {
       path: 'home',
-      name: 'Home'
+      name: 'Home',
     },
     {
       path: 'books',
-      name: 'Books'
+      name: 'Books',
     },
     {
       path: 'about',
-      name: 'About'
+      name: 'About',
     },
-  ]
+  ];
 
-  toggleBurgerMenu(): void{
+  ngOnInit(): void {
+  }
+
+  toggleBurgerMenu(): void {
     if (!this.burgerActive || this.XActive) {
       this.burgerActive = true;
       this.XActive = false;
       if (this.burgerActive) {
         this.renderer.addClass(document.body, 'show-burger');
       }
-    }else{
+    } else {
       this.burgerActive = false;
       this.XActive = true;
       if (this.XActive) {
